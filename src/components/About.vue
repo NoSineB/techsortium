bc644425149bcc951d839f922f3309debc250524
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-05-01 11:35:00 +0530

    First Commit

---
+++ src/components/About.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="about">
+        <div class="w-[70%] h1">
+            <h1 class="text-8xl text-black ms-20 pt-20 font-bold">IEEE Kerala Section</h1>
+            <h1 class="text-8xl text-black ms-20 font-bold">Student Technical</h1>
+            <h1 class="text-8xl text-black ms-20 font-bold">Consortium 2024</h1>
+        </div>
+        <div class="logos">
+            <img src="../assets/images/HEADER.svg" alt="">
+        </div>
+    </div>
+</template>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');
+    .about{
+        height: 100vh;
+        color: black;
+        position: relative;
+    }
+    .h1 h1{
+        font-family: "Russo One";
+    }
+    .logos{
+        position: absolute;
+        bottom: 130px;
+        right: 130px;
+    }
+
+</style>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="container-div">
+            <div class="landing-div">
+                <Landing />
+            </div>
+            <div class="about-div">
+                <About />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import Landing from '../components/Landing.vue';
+    import About from '../components/About.vue';
+</script>
+
+<style>
+    .landing-div{
+        position: sticky;
+        top: 0;
+        left: 0;
+        z-index: -1;
+    }
+    .about-div{
+        background-color: white;
+    }
+</style>
