6262f8f4c6c610638c01a97b0ed64c75697ca382
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-06-05 16:24:34 +0530

    404 Added

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,7 +12,7 @@
                     </p>
                 </div>
                 <div class="">
-                    <p class="text-sm sm:block md:text-base flex items-center mt-2">
+                    <p class="text-sm sm:block md:text-base flex items-center mt-4">
                         <span class="inline-block me-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="" viewBox="0 0 16 16">
                                 <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-[#001b1f] h-screen flex justify-center items-center">
+        <p class="text-white/60 text-2xl">
+            404 | Not Found
+        </p>
+    </div>
+</template>
