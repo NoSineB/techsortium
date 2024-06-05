b26d5aa8afe8346a5bea60da514527aca6cf7d9a
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-06-05 21:12:50 +0530

    Router 404 Fixed

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/:notFound',
+      path: '/:pathMatch(.*)*',
       name: '404',
       component: NotFoundView
     },
